5fcf78efc020145961417c7f5fb46297c8b23fb3
julianafracaroli <julianacfracaroli@gmail.com>
2019-07-08 11:11:54 -0300

    Criação de mais telas

---
+++ app/src/main/java/com/example/aluno/leaveprojeto/tela_selecao.java
@@ -0,0 +1,13 @@
+package com.example.aluno.leaveprojeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tela_selecao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_selecao);
+    }
+}
---
+++ app/src/main/java/com/example/aluno/leaveprojeto/vender_roupa.java
@@ -0,0 +1,13 @@
+package com.example.aluno.leaveprojeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vender_roupa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vender_roupa);
+    }
+}
