e7281ba1895b8a6edb5597c97486ed50b928d970
vini <viniciusifpr15@gmai.com>
2019-08-19 12:07:57 -0300

    linkando telas.

--- app/src/main/java/com/example/aluno/leaveprojeto/LoginActivity.java
+++ app/src/main/java/com/example/aluno/leaveprojeto/LoginActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView tvCadastro;
+    private Button btEntrar;
 
 
     @Override
@@ -26,6 +28,15 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        btEntrar = findViewById(R.id.btEntrar);
+
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, tela_selecao.class));
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/aluno/leaveprojeto/tela_selecao.java
+++ app/src/main/java/com/example/aluno/leaveprojeto/tela_selecao.java
@@ -1,13 +1,29 @@
 package com.example.aluno.leaveprojeto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class tela_selecao extends AppCompatActivity {
+    private Button btVender;
+    private Button btComprar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_selecao);
+        setContentView(R.layout.activity_tela_login);
+
+        btComprar = findViewById(R.id.btComprar);
+
+        btComprar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(tela_selecao.this,LoginActivity.class));
+            }
+        });
+
     }
 }
