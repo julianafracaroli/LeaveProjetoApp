0068a773ce95de27a0226ee52e349ec625e05c9d
vini <viniciusifpr15@gmail.com>
2019-10-31 11:59:54 -0300

    metade login

--- app/src/main/java/com/example/aluno/leaveprojeto/LoginActivity.java
+++ app/src/main/java/com/example/aluno/leaveprojeto/LoginActivity.java
@@ -5,12 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.aluno.leaveprojeto.models.User;
+import com.orm.query.Condition;
+import com.orm.query.Select;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView tvCadastro;
     private Button btEntrar;
+    private EditText etNome, etSenha;
 
 
     @Override
@@ -19,6 +26,11 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tela_login);
 
         tvCadastro = findViewById(R.id.tv_cadastro_login);
+        btEntrar = findViewById(R.id.btEntrar);
+        etNome = findViewById(R.id.etNome);
+        etSenha = findViewById(R.id.etSenha);
+
+
 
         tvCadastro.setOnClickListener(new View.OnClickListener() {
 
@@ -28,13 +40,22 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        btEntrar = findViewById(R.id.btEntrar);
 
         btEntrar.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, tela_selecao.class));
+                String nome = etNome.getText().toString().trim();
+                String senha = etSenha.getText().toString().trim();
+                if (nome.isEmpty() || senha.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Preencha todos os campos.",
+                            Toast.LENGTH_SHORT).show();
+
+                } else {
+                    User user = (Select.from(User.class).where(Condition.prop("login").eq(nome)).and(Condition.prop("senha").eq(senha)).list()).get(0);
+                }
+
+
             }
         });
 
