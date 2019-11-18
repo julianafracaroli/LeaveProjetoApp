f1900ef08646c589ef3c7fac2922d794f01dff0b
vini <viniciusifpr15@gmail.com>
2019-11-18 13:00:55 -0200

    - login corrigido parte foto

--- app/src/main/java/com/example/aluno/leaveprojeto/vender_roupa.java
+++ app/src/main/java/com/example/aluno/leaveprojeto/vender_roupa.java
@@ -2,12 +2,28 @@ package com.example.aluno.leaveprojeto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class vender_roupa extends AppCompatActivity {
+    private Button cam;
+    private EditText etTipoRoupa;
+    private EditText etTamRoupa;
+    private Button btSalvar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vender_roupa);
+
+        cam = findViewById(R.id.cam);
+        btSalvar = findViewById(R.id.btSalvar);
+        etTamRoupa = findViewById(R.id.etTamRoupa);
+        etTipoRoupa = findViewById(R.id.etTipoRoupa);
+
+
+
     }
 }
