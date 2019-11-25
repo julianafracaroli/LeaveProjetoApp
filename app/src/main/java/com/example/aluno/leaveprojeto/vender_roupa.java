a6b1b0a2afc244b315119bc8820dcbf2399389b8
dsadsad <fdsfs@gmail.com>
2019-11-25 13:11:32 -0200

    gggggggggggggggggggggggggggggggggggggggggggfgudeu

--- app/src/main/java/com/example/aluno/leaveprojeto/vender_roupa.java
+++ app/src/main/java/com/example/aluno/leaveprojeto/vender_roupa.java
@@ -1,9 +1,14 @@
 package com.example.aluno.leaveprojeto;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 
 public class vender_roupa extends AppCompatActivity {
@@ -11,19 +16,30 @@ public class vender_roupa extends AppCompatActivity {
     private EditText etTipoRoupa;
     private EditText etTamRoupa;
     private Button btSalvar;
+    private ImageView imgCamera;
+
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vender_roupa);
 
+
+        setContentView(R.layout.activity_vender_roupa);
+        imgCamera = findViewById(R.id.imgCamera);
         cam = findViewById(R.id.cam);
         btSalvar = findViewById(R.id.btSalvar);
         etTamRoupa = findViewById(R.id.etTamRoupa);
         etTipoRoupa = findViewById(R.id.etTipoRoupa);
 
+        cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //tirarfoto();
+            }
 
-
+        });
+        //return imgCamera();
     }
+
 }
